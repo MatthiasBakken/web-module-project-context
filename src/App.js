4fd225bbee434363a13ffc3e9c53d7b18accf364
MatthiasBakken <36183902+MatthiasBakken@users.noreply.github.com>
2021-05-27 03:13:34 -0700

    project finished with context, remove item and localstorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -7,26 +7,51 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Contexts
+import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
+
+
 function App() {
 	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+	const [ cart, setCart ] = useState( [] );
+	
+	useEffect( () => {
+		console.log( 'localStorage', localStorage );
+		if ( localStorage.getItem( "cart" ) ) {
+			const parsedCart = JSON.parse( localStorage.getItem( "cart" ) );
+			setCart( parsedCart );
+			return;
+		} else {
+			return;
+		}
+	}, [] );
 
+	console.log( 'cart empty:', cart );
 	const addItem = item => {
 		// add the given item to the cart
+		const newCart = [ ...cart, item ];
+		localStorage.setItem( "cart", JSON.stringify( newCart ) );
+		setCart( [...cart, item] );
+		console.log( 'cart with items: ', cart );
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
-
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
-
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+			<ProductContext.Provider value={[ products, addItem ]} >
+				<CartContext.Provider value={[cart]} >
+					<Navigation />
+				</CartContext.Provider>
+				{/* Routes */}
+				<Route exact path="/">
+					<Products />
+				</Route>
+				<CartContext.Provider value={[cart, setCart]} >
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from "../contexts/CartContext";
+
 
 const Item = props => {
+	const [ cart, setCart ] = useContext( CartContext );
+
+	const removeFromCartHandler = (id) => {
+		const newCart = cart.filter( item => item.id !== id )
+		localStorage.setItem( "cart", JSON.stringify(newCart) );
+		setCart( newCart );
+	}
+ 
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
@@ -9,7 +19,7 @@ const Item = props => {
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<button onClick={() => removeFromCartHandler(props.id)}>Remove from cart</button>
 			</div>
 		</div>
 	);
